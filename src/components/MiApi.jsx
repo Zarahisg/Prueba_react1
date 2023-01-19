61762d58a7929e77de35ee624c237e22c46f86f6
Zara_G <consultanutriguzman@gmail.com>
2023-01-19 18:43:12 -0300

    commit final

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,41 +1,39 @@
-
 import { useEffect, useState } from "react";
 
 const MiApi = () => {
+  const [locations, setLocations] = useState([]);
+  const [search, setSearch] = useState("");
 
-    const [locations,setLocations] = useState([]);
-    const [search,setSearch]= useState('')
 
-    const getData = async () => {
-       const res= await fetch (`https://rickandmortyapi.com/api/location`)
-       const data = await res.json();
-       setLocations(data.results);
-    };
-   
+  const getData = async () => {
+    const res = await fetch(`https://rickandmortyapi.com/api/location`);
+    const data = await res.json();
+    setLocations(data.results);
+  };
 
-  useEffect (() => {
+  useEffect(() => {
     getData();
-  },[])
+  }, []);
   return (
+    
     <div>
-        <h3 className="text-primary text-center">
-            Elige tu locación Favorita</h3>
-        <input 
+      <h3 className="text-primary text-center">Elige tu locación Favorita</h3>
+      <input
         className="input-group mb-3"
         type="text"
-        placeholder="buscar..." 
-        onChange={(e) => setSearch(e.target.value)} 
-        value= {search}
-         />
+        placeholder="buscar..."
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
 
-        <ul className="p-3 mb-2 bg-info text-dark text-center">
+      <ul className="p-3 mb-2 bg-info text-dark text-center">
         {locations
-        .filter((x)=> x.name.toLowerCase().includes(search.toLowerCase()))
-        .map((item) => (
-           <li key={item.id}>{item.name}</li>
-            ))}
-        </ul>
+          .filter((x) => x.name.toLowerCase().includes(search.toLowerCase()))
+          .map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+      </ul>
     </div>
   );
-  };
-  export default MiApi;
+};
+export default MiApi;
